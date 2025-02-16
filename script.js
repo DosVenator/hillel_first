9e9a7de2f53a9429f5ee6d3d26f96e9320922453
DosVenator <wadocraft@gmail.com>
2025-02-16 16:30:46 +0200

    feat:first_page

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-const num = 13;
-const str = 'Hello developer';
-const BigI = 1234567890123456789012345678901234567890n;
-let und;
-console.log(num);
-console.log(str);
-console.log(und);
-console.log(null);
+// const num = 13;
+// const str = 'Hello developer';
+// const BigI = 1234567890123456789012345678901234567890n;
+// let und;
+// console.log(num);
+// console.log(str);
+// console.log(und);
+// console.log(null);
 
-function example(num) {
-    return num % 2 === 0;
-}
+// function example(num) {
+//     return num % 2 === 0;
+// }
 
-console.log(example(10));
-console.log(example(7));  
-let arr1 = ['Oleg','Egor', 'Vadim', 'Mukolai', 'Olecsiy',];
-console.log(typeof arr1);
-console.log(typeof num);
-console.log(typeof str);
-console.log(typeof BigI);
+// console.log(example(10));
+// console.log(example(7));  
+// let arr1 = ['Oleg','Egor', 'Vadim', 'Mukolai', 'Olecsiy',];
+// console.log(typeof arr1);
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof BigI);
 
-const AgeResult = document.querySelector('.descr');
-const CityResult = document.querySelector('.bottom');
-let age = prompt('How old are you?');
-let city = prompt('Whe are you from?');
-AgeResult.innerHTML = "You are: " + age;
-CityResult.innerHTML = "Your city: " + city;
-console.log(`${age} ${city}`);
+// const AgeResult = document.querySelector('.descr');
+// const CityResult = document.querySelector('.bottom');
+// let age = prompt('How old are you?');
+// let city = prompt('Whe are you from?');
+// AgeResult.innerHTML = "You are: " + age;
+// CityResult.innerHTML = "Your city: " + city;
+// console.log(`${age} ${city}`);
 
-// Тут допоміг ChatGPT
-let number = prompt("Enter a five-digit number:");
-    console.log(number.split("").join(" "));
+// // Тут допоміг ChatGPT
+// let number = prompt("Enter a five-digit number:");
+//     console.log(number.split("").join(" "));
 
-let lines = [
-    prompt("Введіть перший рядок:"),
-    prompt("Введіть другий рядок:"),
-    prompt("Введіть третій рядок:")
-];
+// let lines = [
+//     prompt("Введіть перший рядок:"),
+//     prompt("Введіть другий рядок:"),
+//     prompt("Введіть третій рядок:")
+// ];
     
-console.log(lines.sort(() => Math.random() - 0.5).join("\n"));
+// console.log(lines.sort(() => Math.random() - 0.5).join("\n"));
 
