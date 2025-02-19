99a1e5a7662e431eea948c8fd2c8d2ea96fe9ab2
DosVenator <wadocraft@gmail.com>
2025-02-19 20:25:40 +0200

    feat:home_work5

--- lesson_5/homescript5.js
+++ lesson_5/homescript5.js
@@ -1,44 +1,44 @@
-// let userNum;
-// let minNum = 0;
-// let flag_1 = false;
-// let i = 1;
-// let degree = 0;
-// let startNum = 1;
-// let flag_2 = false;
+let userNum;
+let minNum = 0;
+let flag_1 = false;
+let i = 1;
+let degree = 0;
+let startNum = 1;
+let flag_2 = false;
 
-// do {
-// userNum = parseInt(prompt('Enter digit bigger than ' + 1));
-// } while (userNum < 1);
+do {
+userNum = parseInt(prompt('Enter digit bigger than ' + 1));
+} while (userNum < 1);
 
-// /* спочатку вивів такий код, але зіштовхнувся з проблемою відображення самогу ступеня, при якому ми маємо даний результат */
+/* спочатку вивів такий код, але зіштовхнувся з проблемою відображення самогу ступеня, при якому ми маємо даний результат */
 
-// // for(i = 1; i < userNum; i++) {
-// //     let result = i * 3;
-// //     if (result === userNum) {
-// //         flag_1 = true;
-// //         break;
-// //     } 
-// //     console.log(result)
-// // }
-// // if(flag_1) {
-// //     document.write(`<p>This number can be obtained by raising the number 3 to the power</p>`)
-// // } else {
-// //     document.write(`<p>This number cannot be obtained by raising the number 3 to the power</p>`)
-// // }
-
-
-
-// while (startNum <= userNum) {
-//     if (startNum === userNum) {
-//         flag_2 = true;
+// for(i = 1; i < userNum; i++) {
+//     let result = i * 3;
+//     if (result === userNum) {
+//         flag_1 = true;
 //         break;
-//     }
-//     startNum *= 3;
-//     degree++;
+//     } 
+//     console.log(result)
+// }
+// if(flag_1) {
+//     document.write(`<p>This number can be obtained by raising the number 3 to the power</p>`)
+// } else {
+//     document.write(`<p>This number cannot be obtained by raising the number 3 to the power</p>`)
 // }
 
-// if (flag_2) {
-//     document.write(`<p>This number can be obtained by raising to the power of ${degree}th</p>`)
-// } else {
-//     document.write('<p>This number cannot be obtained when raised to the 3th power</p>')
-// }
+
+
+while (startNum <= userNum) {
+    if (startNum === userNum) {
+        flag_2 = true;
+        break;
+    }
+    startNum *= 3;
+    degree++;
+}
+
+if (flag_2) {
+    document.write(`<p>This number can be obtained by raising to the power of ${degree}th</p>`)
+} else {
+    document.write('<p>This number cannot be obtained when raised to the 3th power</p>')
+}
