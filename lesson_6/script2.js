ba4037bc436a6a4ec10a5e8bea16fdd11db1b35e
DosVenator <wadocraft@gmail.com>
2025-02-23 22:37:03 +0200

    feat:home_work6_fix

--- lesson_6/script2.js
+++ lesson_6/script2.js
@@ -103,17 +103,17 @@ const arr = [16,-37,54,-4,72,-56,47,4,-16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,
 
 //j
 
-let maxDigit = arr[0];
-for(let i = 0; i < arr.length; i++) {
-    if (arr[i] > maxDigit) {
-        maxDigit = arr[i];
-    }
-}
-for(let i = 0; i < arr.length; i++) {
-    if (arr[i] !== maxDigit) {
-        arr[i] = 0;
-    }
-}
-console.log(arr);
-console.log(maxDigit);
+// let maxDigit = arr[0];
+// for(let i = 0; i < arr.length; i++) {
+//     if (arr[i] > maxDigit) {
+//         maxDigit = arr[i];
+//     }
+// }
+// for(let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== maxDigit) {
+//         arr[i] = 0;
+//     }
+// }
+// console.log(arr);
+// console.log(maxDigit);
 
