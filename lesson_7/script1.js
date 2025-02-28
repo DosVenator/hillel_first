0ab3c6ff196cdc89411c029cf5cee523a760ab0b
DosVenator <wadocraft@gmail.com>
2025-02-28 15:21:10 +0200

    feat:fix_home7

--- lesson_7/script1.js
+++ lesson_7/script1.js
@@ -1,25 +1,25 @@
-// let user;
-// do {
-// user = prompt('Enter any string')
-// } while (!isNaN(parseInt(user)));
-// user = user.split('');
+let user;
+do {
+  user = prompt("Enter any string");
+} while (!isNaN(parseInt(user)));
+user = user.split("");
 
-// let delet;
-// do {
-// delet = prompt('Enter any symbol for delete');
-// } while (delet === null)
-// dele = delet.split('');
+let delet;
+do {
+  delet = prompt("Enter any symbol for delete");
+} while (delet === null);
+dele = delet.split("");
 
-// function deleteA (a, b) {
-//     for(let i = 0; i < a.length; i++) {
-//         for(let e = 0; e < b.length; e++) {
-//             if (a[i] === b[e]) {
-//                 a.splice(i, 1);
-//                 i--;
-//             }
-//         }
-//     }
-//     console.log(a.join(''));
-// }
+function deleteA(a, b) {
+  for (let i = 0; i < a.length; i++) {
+    for (let e = 0; e < b.length; e++) {
+      if (a[i] === b[e]) {
+        a.splice(i, 1);
+        i--;
+      }
+    }
+  }
+  console.log(a.join(""));
+}
 
-// deleteA (user, delet);
+deleteA(user, delet);
--- lesson_7/script3.js
+++ lesson_7/script3.js
@@ -1,19 +1,19 @@
 let user;
 do {
-user = prompt('Enter any string')
+  user = prompt("Enter any string");
 } while (user === null);
-user = user.split('');
-console.log(user)
+user = user.split("");
+console.log(user);
 
-function deleteA (a, b) {
-    b = String(b);
-    for(let i = 0; i < a.length; i++) {
-        if (a[i] === b) {
-            a.splice(i, 1);
-            i--;
-        }
+function deleteA(a, b) {
+  b = String(b);
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === b) {
+      a.splice(i, 1);
+      i--;
     }
-    console.log(a.join(''));
+  }
+  console.log(a.join(""));
 }
 
-deleteA (user, 2);
+deleteA(user, 2);
