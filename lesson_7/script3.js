0457616797c9222d1161213bb8ddc06a37a2d577
DosVenator <wadocraft@gmail.com>
2025-03-01 13:55:41 +0200

    feat:fix_home6_3

--- lesson_7/script3.js
+++ lesson_7/script3.js
@@ -1,19 +1,14 @@
-let user;
-do {
-  user = prompt("Enter any string");
-} while (user === null);
-user = user.split("");
-console.log(user);
+let user = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function deleteA(a, b) {
-  b = String(b);
   for (let i = 0; i < a.length; i++) {
     if (a[i] === b) {
       a.splice(i, 1);
       i--;
     }
   }
-  console.log(a.join(""));
 }
 
-deleteA(user, 2);
+deleteA(user, 8);
+
+console.log(user);
