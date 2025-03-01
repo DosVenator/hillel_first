968d8684b26510c77c38e77a1d6d3784a1916c48
DosVenator <wadocraft@gmail.com>
2025-03-01 15:28:20 +0200

    feat:home_work8_task3

---
+++ lesson_8/script3.js
@@ -0,0 +1,23 @@
+function getNumber() {
+  let userValue;
+  for (let i = 0; i < 4; i++) {
+    userValue = prompt("Enter a number bigger than 100");
+    if (userValue === null) {
+      console.log("User closed input");
+      return;
+    }
+    userValue = Number(userValue);
+    if (isNaN(userValue)) {
+      userValue = "User entered is not a number";
+      console.log("Invalid input. Please, enter a number");
+    } else if (userValue > 100) {
+      console.log(
+        `User entered  a number bigger than 100, this is: ${userValue}`
+      );
+      return;
+    }
+  }
+  console.log(`Your attemps are over, last value: ${userValue}`);
+}
+
+getNumber();
