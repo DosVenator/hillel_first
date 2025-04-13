a9b2baaf614a549a72abed6c43df15fc8eef94cf
DosVenator <wadocraft@gmail.com>
2025-04-14 00:02:04 +0300

    feat:global_page19

--- lesson_19/script/LMS_timer.js
+++ lesson_19/script/LMS_timer.js
@@ -1,28 +1,25 @@
-const parent = document.querySelector('.timer');
-const btnTimer = document.querySelector('.btn_timer');
+const parent = document.querySelector(".timer");
+const btnTimer = document.querySelector(".btn_timer");
 let startTime;
-
-
-
 let timerId;
 
-function timer () {
-    timerId = setInterval(() => {
-        startTime--;
-        const minut = Math.floor(startTime / 60);
-        const second = startTime % 60;
-        parent.textContent = `${minut.toString().padStart(2, "0")}:${second.toString().padStart(2, "0")}`;
-        if(startTime <= 0) {
-            clearInterval(timerId);
-            parent.textContent = "Timer is over";
-        }
-    }, 1000)
-    
-    
+function timer() {
+  timerId = setInterval(() => {
+    startTime--;
+    const minut = Math.floor(startTime / 60);
+    const second = startTime % 60;
+    parent.textContent = `${minut.toString().padStart(2, "0")}:${second
+      .toString()
+      .padStart(2, "0")}`;
+    if (startTime <= 0) {
+      clearInterval(timerId);
+      parent.textContent = "Timer is over";
+    }
+  }, 1000);
 }
 
-btnTimer.addEventListener('click', () => {
-    clearInterval(timerId);
-    startTime = 85;
-    timer();
-})
+btnTimer.addEventListener("click", () => {
+  clearInterval(timerId);
+  startTime = 85;
+  timer();
+});
